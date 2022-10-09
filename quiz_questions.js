115abdb53a10e4488132c3234cec70892c378d99
StayAbroad <dennis.koester11@gmail.com>
2022-10-09 18:33:44 +0200

    change choose quiz

---
+++ quiz_questions.js
@@ -0,0 +1,141 @@
+let questionsHTML = [
+    {
+        "question": "Who developed HTML?",
+        "answer_1": "Robbie-Williams",
+        "answer_2": "Lady-Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+
+    {
+        "question": "What does HTML stand for?",
+        "answer_1": "Hyper Trainer Marking Language",
+        "answer_2": "Hyper Text Markup Language",
+        "answer_3": "Hyper Text Marketing Language",
+        "answer_4": "Hyper Text Markup Leveler",
+        "right_answer": 2
+    },
+
+    {
+        "question": "Choose the correct HTML element for the largest heading:",
+        "answer_1": "&lt;h6&gt;",
+        "answer_2": "&lt;h1&gt;",
+        "answer_3": "&lt;heading&gt;",
+        "answer_4": "&lt;head&gt;",
+        "right_answer": 2
+    },
+
+    {
+        "question": "What is the correct HTML element for inserting a line break?",
+        "answer_1": "&lt;br&gt;",
+        "answer_2": "&lt;break&gt;",
+        "answer_3": "&lt;lb&gt;",
+        "answer_4": "&lt;bl&gt;",
+        "right_answer": 1
+    },
+
+    {
+        "question": "How can you make a numbered list?",
+        "answer_1": "&lt;list&gt;",
+        "answer_2": "&lt;dl&gt;",
+        "answer_3": "&lt;ul&gt;",
+        "answer_4": "&lt;ol&gt;",
+        "right_answer": 4
+    },
+];
+
+let questionsCSS = [
+    {
+        "question": "What does CSS stand for?",
+        "answer_1": "Computer Style Sheets",
+        "answer_2": "Creative Style Sheets",
+        "answer_3": "Cascading Style Sheets",
+        "answer_4": "Colorful Style Sheets",
+        "right_answer": 3
+    },
+
+    {
+        "question": "What is the correct HTML for referring to an external style sheet?",
+        "answer_1": "&lt;stylesheet&gt;mystyle.css&lt;/stylesheet&gt;",
+        "answer_2": "&lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href=&quot;mystyle.css&quot;&gt;",
+        "answer_3": "&lt;style src=&quot;mystyle.css&quot;&gt;",
+        "answer_4": "&lt;style&gt;mystyle.css&lt;/style&gt;",
+        "right_answer": 2
+    },
+
+    {
+        "question": "Where in an HTML document is the correct place to refer to an external style sheet?",
+        "answer_1": "styles",
+        "answer_2": "class",
+        "answer_3": "font",
+        "answer_4": "style",
+        "right_answer": 4
+    },
+
+    {
+        "question": "Which HTML attribute is used to define inline styles?",
+        "answer_1": "In the &lt;body&gt; section",
+        "answer_2": "In the &lt;head&gt; section",
+        "answer_3": "At the end of the document",
+        "answer_4": "In the &lt;footer&gt; section",
+        "right_answer": 2
+    },
+
+    {
+        "question": "Which is the correct CSS syntax?",
+        "answer_1": "body:color=black;",
+        "answer_2": "{body:color=black;}",
+        "answer_3": "{body;color=black;}",
+        "answer_4": "body {color: black;}",
+        "right_answer": 4
+    },
+]
+
+let questionsJS = [
+    {
+        "question": "Inside which HTML element do we put the JavaScript?",
+        "answer_1": "&lt;js&gt;",
+        "answer_2": "&lt;javascript&gt;",
+        "answer_3": "&lt;scripting&gt;",
+        "answer_4": "&lt;script&gt;",
+        "right_answer": 4
+    },
+
+    {
+        "question": "What is the correct syntax for referring to an external script called &quot;xxx.js&quot;?",
+        "answer_1": "&lt;script name=&quot;xxx.js&chot;&gt;",
+        "answer_2": "&lt;script src=&chot;xxx.js&chot;&gt;",
+        "answer_3": "&lt;script href=&chot;xxx.js&chot;&gt;",
+        "answer_4": "&lt;script url=&chot;xxx.js&chot;&gt;",
+        "right_answer": 2
+    },
+
+    {
+        "question": `What is the correct JavaScript syntax to change the content of the HTML element below?<br><br>
+        "&lt;p id=&quot;demo&quot;&gt;This is a demonstration.&lt;/p&gt;;`,
+        "answer_1": `#demo.innerHTML = &quot;Hello World!&quot;`,
+        "answer_2": `document.getElementByName(&quot;p&quot;).innerHTML = &quot;Hello World!&quot;;`,
+        "answer_3": `document.getElement(&quot;p&quot;).innerHTML = &quot;Hello World!&quot;;`,
+        "answer_4": `document.getElementById(&quot;demo&quot;).innerHTML = &quot;Hello World!&quot;;`,
+        "right_answer": 4
+    },
+
+    {
+        "question": "How do you create a function in JavaScript?",
+        "answer_1": "function myFunction()",
+        "answer_2": "function:myFunction()",
+        "answer_3": "function = myFunction()",
+        "answer_4": "function = {myFunction()}",
+        "right_answer": 2
+    },
+
+    {
+        "question": "How to write an IF statement in JavaScript?",
+        "answer_1": "if i == 5 then",
+        "answer_2": "if (i == 5)",
+        "answer_3": "if i = 5",
+        "answer_4": "if i = 5 then",
+        "right_answer": 2
+    },
+]
--- script.js
+++ script.js
@@ -1,163 +1,38 @@
-let questionsHTML = [
-    {
-        "question": "Who developed HTML?",
-        "answer_1": "Robbie-Williams",
-        "answer_2": "Lady-Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
-        "right_answer": 3
-    },
-
-    {
-        "question": "What does HTML stand for?",
-        "answer_1": "Hyper Trainer Marking Language",
-        "answer_2": "Hyper Text Markup Language",
-        "answer_3": "Hyper Text Marketing Language",
-        "answer_4": "Hyper Text Markup Leveler",
-        "right_answer": 2
-    },
-
-    {
-        "question": "Choose the correct HTML element for the largest heading:",
-        "answer_1": "&lt;h6&gt;",
-        "answer_2": "&lt;h1&gt;",
-        "answer_3": "&lt;heading&gt;",
-        "answer_4": "&lt;head&gt;",
-        "right_answer": 2
-    },
-
-    {
-        "question": "What is the correct HTML element for inserting a line break?",
-        "answer_1": "&lt;br&gt;",
-        "answer_2": "&lt;break&gt;",
-        "answer_3": "&lt;lb&gt;",
-        "answer_4": "&lt;bl&gt;",
-        "right_answer": 1
-    },
-
-    {
-        "question": "How can you make a numbered list?",
-        "answer_1": "&lt;list&gt;",
-        "answer_2": "&lt;dl&gt;",
-        "answer_3": "&lt;ul&gt;",
-        "answer_4": "&lt;ol&gt;",
-        "right_answer": 4
-    },
-];
-
-let questionCSS = [
-    {
-        "question": "What does CSS stand for?",
-        "answer_1": "Computer Style Sheets",
-        "answer_2": "Creative Style Sheets",
-        "answer_3": "Cascading Style Sheets",
-        "answer_4": "Colorful Style Sheets",
-        "right_answer": 3
-    },
-
-    {
-        "question": "What is the correct HTML for referring to an external style sheet?",
-        "answer_1": "&lt;stylesheet&gt;mystyle.css&lt;/stylesheet&gt;",
-        "answer_2": "&lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href=&quot;mystyle.css&quot;&gt;",
-        "answer_3": "&lt;style src=&quot;mystyle.css&quot;&gt;",
-        "answer_4": "&lt;style&gt;mystyle.css&lt;/style&gt;",
-        "right_answer": 2
-    },
-
-    {
-        "question": "Where in an HTML document is the correct place to refer to an external style sheet?",
-        "answer_1": "styles",
-        "answer_2": "class",
-        "answer_3": "font",
-        "answer_4": "style",
-        "right_answer": 4
-    },
-
-    {
-        "question": "Which HTML attribute is used to define inline styles?",
-        "answer_1": "In the &lt;body&gt; section",
-        "answer_2": "In the &lt;head&gt; section",
-        "answer_3": "At the end of the document",
-        "answer_4": "In the &lt;footer&gt; section",
-        "right_answer": 2
-    },
-
-    {
-        "question": "Which is the correct CSS syntax?",
-        "answer_1": "body:color=black;",
-        "answer_2": "{body:color=black;}",
-        "answer_3": "{body;color=black;}",
-        "answer_4": "body {color: black;}",
-        "right_answer": 4
-    },
-]
-
-let questionsJS = [
-    {
-        "question": "Inside which HTML element do we put the JavaScript?",
-        "answer_1": "&lt;js&gt;",
-        "answer_2": "&lt;javascript&gt;",
-        "answer_3": "&lt;scripting&gt;",
-        "answer_4": "&lt;script&gt;",
-        "right_answer": 4
-    },
-
-    {
-        "question": "What is the correct syntax for referring to an external script called &quot;xxx.js&quot;?",
-        "answer_1": "&lt;script name=&quot;xxx.js&chot;&gt;",
-        "answer_2": "&lt;script src=&chot;xxx.js&chot;&gt;",
-        "answer_3": "&lt;script href=&chot;xxx.js&chot;&gt;",
-        "answer_4": "&lt;script url=&chot;xxx.js&chot;&gt;",
-        "right_answer": 2
-    },
-
-    {
-        "question": `What is the correct JavaScript syntax to change the content of the HTML element below?<br><br>
-        "&lt;p id=&quot;demo&quot;&gt;This is a demonstration.&lt;/p&gt;;`,
-        "answer_1": `#demo.innerHTML = &quot;Hello World!&quot;`,
-        "answer_2": `document.getElementByName(&quot;p&quot;).innerHTML = &quot;Hello World!&quot;;`,
-        "answer_3": `document.getElement(&quot;p&quot;).innerHTML = &quot;Hello World!&quot;;`,
-        "answer_4": `document.getElementById(&quot;demo&quot;).innerHTML = &quot;Hello World!&quot;;`,
-        "right_answer": 4
-    },
-
-    {
-        "question": "How do you create a function in JavaScript?",
-        "answer_1": "function myFunction()",
-        "answer_2": "function:myFunction()",
-        "answer_3": "function = myFunction()",
-        "answer_4": "function = {myFunction()}",
-        "right_answer": 2
-    },
-
-    {
-        "question": "How to write an IF statement in JavaScript?",
-        "answer_1": "if i == 5 then",
-        "answer_2": "if (i == 5)",
-        "answer_3": "if i = 5",
-        "answer_4": "if i = 5 then",
-        "right_answer": 2
-    },
-]
-
 let currentQuestion = 0;
 let rightQuestions = 0
 let AUDIO_SUCCESS = new Audio('/audio/success.mp3');
 let AUDIO_FAIL = new Audio('/audio/fail.mp3');
 
 function init() {
-    document.getElementById('all-questions').innerHTML = questionsHTML.length;
-    showQuestion();
+    showStartScreen();
 }
 
-function showQuestion() {
+function startQuiz(quiz) {
+    showQuestionScreen();
+    showQuestions(quiz);
+}
 
-    if (gameIsOver()) {
+function chooseQuiz(quiz) {
+    chosenquiz = quiz;
+    if (chosenquiz == 'questionsHTML') {
+        startQuiz(quiz);
+    }
+    if (chosenquiz == 'questionsCSS') {
+        startQuiz(quiz);
+    }
+    if (chosenquiz == 'questionsJS') {
+        startQuiz(quiz);
+    }
+}
+
+function showQuestions(quiz) {
+
+    if (gameIsOver(quiz)) {
         // console.log('DONE')
         showEndScreen();
     } else {
-        updateProgressBar()
-        updateNextQuestion();
+        updateProgressBar(quiz)
+        updateNextQuestion(quiz);
     }
 }
 
@@ -213,7 +88,7 @@ function nextQuestion() {
     document.getElementById('next-btn').disabled = true;
     resetAnswerButtons();
     resetOnclick()
-    showQuestion();
+    showQuestions();
 }
 
 function resetAnswerButtons() {
@@ -243,6 +118,9 @@ function restartQuiz() {
     document.getElementById('header-img').src = '/Quizapp Design/quiz-night.png'; // Changes the image
     document.getElementById('endScreen').style = 'display: none;'; // hide the end screen
     document.getElementById('questionBody').style = ''; // displays the questions screen
+    document.getElementById('progress').style = '';
+    document.getElementById('questionBody').style = 'display: none';
+
 
     rightQuestions = 0;
     currentQuestion = 0;
@@ -255,10 +133,11 @@ function showEndScreen() {
     document.getElementById('amountOfQuestions').innerHTML = questionsHTML.length;
     document.getElementById('amountOfRightQuestions').innerHTML = rightQuestions;
     document.getElementById('header-img').src = '/Quizapp Design/trophy-gabff6e205_640.png';
+    document.getElementById('progress').style = 'display: none';
 }
 
-function updateNextQuestion() {
-    let question = questionsHTML[currentQuestion];
+function updateNextQuestion(quiz) {
+    let question = quiz[currentQuestion];
 
     document.getElementById('current-question').innerHTML = currentQuestion + 1;
     document.getElementById('question-text').innerHTML = question['question'];
@@ -268,18 +147,29 @@ function updateNextQuestion() {
     document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
 
-function updateProgressBar() {
-    let percent = (currentQuestion + 1) / questionsHTML.length;
+function updateProgressBar(quiz) {
+    let percent = currentQuestion / quiz.length;
     percent = Math.round(percent * 100);
 
     document.getElementById('progress-bar').innerHTML = `${percent}%`;
     document.getElementById('progress-bar').style = `width: ${percent}%;`;
 }
 
-function gameIsOver() {
-    return currentQuestion >= questionsHTML.length;
+function gameIsOver(quiz) {
+    return currentQuestion >= quiz.length;
 }
 
 function rightAnswerSelected(selectedQuestionNumber, question) {
     return selectedQuestionNumber == question['right_answer'];
+}
+
+function showStartScreen() {
+    document.getElementById('startScreen').style = '';
+    document.getElementById('progress').style = 'display: none';
+}
+
+function showQuestionScreen() {
+    document.getElementById('questionBody').style = '';
+    document.getElementById('startScreen').style = 'display: none';
+    document.getElementById('progress').style = '';
 }
